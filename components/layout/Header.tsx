d79102b571493f7d9264dd11eec02c114c2cdc8e
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2022-06-11 18:26:48 -0300

    📌 feat: inicia desenvolvimento do header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,7 +4,7 @@ import { TitleHeader } from 'styles/layout/Header';
 const Header = () => {
   return (
     <Container.Header>
-      <TitleHeader data-cy="home_page_title">Hello World</TitleHeader>
+      <TitleHeader data-cy="header_logo">Wine</TitleHeader>
     </Container.Header>
   );
 };
